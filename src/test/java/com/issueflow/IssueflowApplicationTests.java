5f3503e523147964a253f227bfd49df8a560b91e
salomanrajus <tremma7258@gmail.com>
2026-09-23 19:48:26 +0530

    chore: initialize Spring Boot project

---
+++ src/test/java/com/issueflow/IssueflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issueflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
